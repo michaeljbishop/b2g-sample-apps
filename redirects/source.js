f982d1eb4dff0713bef3a9fb83cdd252c81a879b
Michael J. Bishop <mbtyke@gmail.com>
2013-07-02 13:02:20 -0400

    Made sure *all* inline javascript was removed.

--- redirects/source.js
+++ redirects/source.js
@@ -1,6 +1,10 @@
-function testRedirectSource(){
-    window.open("http://MichaelPro.local/~mbishop/index.html");
-}
-function testRedirectTarget(){
+window.onload = function () {
+    document.getElementById("open-with-redirection").onclick = function () {
+        window.open("http://MichaelPro.local/~mbishop/index.html");
+    };
+
+    document.getElementById("open-without-redirection").onclick = function () {
     window.open("/redirects/authenticated.html");
+    };
 }
+
